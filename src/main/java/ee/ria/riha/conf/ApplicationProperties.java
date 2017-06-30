e702621a37c3e02affa621442930f8c702a0cf6d
Valentin Suhnjov <valentin.suhnjov@wisercat.eu>
2017-06-30 16:05:56 +0300

    RIHAKB-173. Add CORS configuration

--- src/main/java/ee/ria/riha/conf/ApplicationProperties.java
+++ src/main/java/ee/ria/riha/conf/ApplicationProperties.java
@@ -4,6 +4,9 @@ import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Valentin Suhnjov
  */
@@ -13,6 +16,8 @@ public class ApplicationProperties {
 
     private final Meta meta = new Meta();
     private final StorageClient storageClient = new StorageClient();
+    private final Security security = new Security();
+
     private boolean selfHosted = true;
 
     public Meta getMeta() {
@@ -21,6 +26,10 @@ public class ApplicationProperties {
 
     public StorageClient getStorageClient() {
         return storageClient;
+    }
+
+    public Security getSecurity() {
+        return security;
     }
 
     public boolean isSelfHosted() {
@@ -64,6 +73,26 @@ public class ApplicationProperties {
 
         public void setBaseUrl(String baseUrl) {
             this.baseUrl = baseUrl;
+        }
+    }
+
+    public static class Cors {
+        private final List<String> allowedOrigins = new ArrayList<>();
+
+        public List<String> getAllowedOrigins() {
+            return allowedOrigins;
+        }
+
+        public void setAllowedOrigins(List<String> allowedOrigins) {
+            this.allowedOrigins.addAll(allowedOrigins);
+        }
+    }
+
+    public static class Security {
+        private final Cors cors = new Cors();
+
+        public Cors getCors() {
+            return cors;
         }
     }
 }
--- src/main/java/ee/ria/riha/conf/WebMvcConfiguration.java
+++ src/main/java/ee/ria/riha/conf/WebMvcConfiguration.java
@@ -2,8 +2,10 @@ package ee.ria.riha.conf;
 
 import ee.ria.riha.storage.util.FilterableArgumentResolver;
 import ee.ria.riha.storage.util.PageableArgumentResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
@@ -14,10 +16,22 @@ import java.util.List;
 @Configuration
 public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 
+    @Autowired
+    private ApplicationProperties applicationProperties;
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         super.addArgumentResolvers(argumentResolvers);
         argumentResolvers.add(new PageableArgumentResolver());
         argumentResolvers.add(new FilterableArgumentResolver());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        applicationProperties.getSecurity().getCors().getAllowedOrigins().toArray(new String[0]))
+                .allowedHeaders("*")
+                .allowedMethods("*");
     }
 }
